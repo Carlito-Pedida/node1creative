a9d675c0ab40f2ad89bed4126deed55c2b12e2fe
Carlito Pedida Jr <carlped417@gmail.com>
2024-11-22 15:57:34 -0600

    Cleanup

--- .history/app/page_20241120140752.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-1-7xl w-full">
-        <h1>Hello World!</h1>
-      </div>
-    </main>
-  );
-}
